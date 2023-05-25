a32e117879dc97d18aa0963eb92f356b13affb76
MATHEUS MENDONCA MAIGNARDI <70504309+Maignardi@users.noreply.github.com>
2023-05-24 22:34:11 -0400

    Initial Commit

---
+++ PontualWatch Watch App/PontualWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PontualWatchApp.swift
+//  PontualWatch Watch App
+//
+//  Created by matheus maignardi on 24/05/23.
+//
+
+import SwiftUI
+
+@main
+struct PontualWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
