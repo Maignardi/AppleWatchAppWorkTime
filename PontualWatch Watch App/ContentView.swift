9c37fa2b0ef351244ab767b1e41dbf0b95d555f8
MATHEUS MENDONCA MAIGNARDI <70504309+Maignardi@users.noreply.github.com>
2023-05-26 22:12:47 -0400

    first commit

--- PontualWatch Watch App/ContentView.swift
+++ PontualWatch Watch App/ContentView.swift
@@ -1,21 +1,105 @@
-//
-//  ContentView.swift
-//  PontualWatch Watch App
-//
-//  Created by matheus maignardi on 24/05/23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    @State private var arrivalTime: Date?
+    @State private var isArrivalTimeVisible = true
+
+    private var departureTime: Date? {
+        guard let arrivalTime = arrivalTime else {
+            return nil
         }
-        .padding()
+        let calendar = Calendar.current
+        let endTime = calendar.date(byAdding: .hour, value: 9, to: arrivalTime)!
+        return calendar.date(byAdding: .minute, value: 15, to: endTime)!
+    }
+
+    private var formattedDepartureTime: String {
+        guard let departureTime = departureTime else {
+            return ""
+        }
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm"
+        return formatter.string(from: departureTime)
+    }
+
+    var body: some View {
+        ScrollView {
+            VStack {
+                Spacer()
+
+                Text("Ponto")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .padding()
+                    .frame(maxWidth: .infinity, alignment: .center) // Centralizar o título
+
+                VStack(alignment: .center) {
+                    Text("Horário de Chegada:")
+                        .font(.headline)
+                        .padding(.top)
+
+                    if let arrivalTime = arrivalTime {
+                        Text(getTimeString(from: arrivalTime))
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .padding()
+                    } else {
+                        Button(action: {
+                            arrivalTime = Date()
+                            isArrivalTimeVisible = false
+                        }) {
+                            Text("Registrar Chegada")
+                                .font(.title)
+                                .fontWeight(.bold)
+                                .padding()
+                                .background(Color.blue)
+                                .foregroundColor(.white)
+                                .cornerRadius(10)
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity, alignment: .center) // Centralizar o VStack
+
+                VStack(alignment: .center) {
+                    Text("Horário de Saída:")
+                        .font(.headline)
+                        .padding(.top)
+
+                    Text(formattedDepartureTime)
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.blue)
+                        .padding()
+                }
+                .frame(maxWidth: .infinity, alignment: .center) // Centralizar o VStack
+
+                Spacer()
+
+                Button(action: {
+                    resetArrivalTime()
+                }) {
+                    Text("Resetar")
+                        .font(.footnote)
+                        .foregroundColor(.gray)
+                        .padding()
+                }
+                .frame(maxWidth: .infinity, alignment: .trailing) // Alinhar o botão à direita
+                .padding(.trailing)
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .alignmentGuide(.top) { $0[.bottom] }
+        }
+    }
+
+    private func getTimeString(from date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm"
+        return formatter.string(from: date)
+    }
+
+    private func resetArrivalTime() {
+        arrivalTime = nil
+        isArrivalTimeVisible = true
     }
 }
 
